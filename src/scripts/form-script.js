a271416a4242106b534bebb941707f3cce2fdd4e
Tomin-Joy <tominjk007@gmail.com>
2022-07-03 15:22:34 +0530

    update

--- src/scripts/form-script.js
+++ src/scripts/form-script.js
@@ -1,44 +1,59 @@
+function mailCheck(input) {
+    let reg = /[a-zA-Z]+[0-9]+@[a-zA-Z]+\.sjcetpalai\.ac\.in/i;
+    return reg.test(input);
+}
+
 
 function dataTransfer() {
     const thForm = document.forms['THForm']
+    if(thForm['memid'].value.length==17){
+        if(mailCheck(thForm['email'].value)){
+            let batch
+            switch (thForm['batch'].value) {
+                case '1':
+                    batch = "2022-26"
+                    break;
+                case '2':
+                    batch = "2021-25"
+                    break;
+                case '3':
+                    batch = "2020-24"
+                    break;
+                case '4':
+                    batch = "2019-23"
+                    break;
+                default:
+                    break;
+            }
 
-    let batch
-    switch (thForm['batch'].value) {
-        case '1':
-            batch = "2022-26"
-            break;
-        case '2':
-            batch = "2021-25"
-            break;
-        case '3':
-            batch = "2020-24"
-            break;
-        case '4':
-            batch = "2019-23"
-            break;
-        default:
-            break;
+            var JSONData = {
+                "memid": thForm['memid'].value,
+                "name": thForm['name'].value,
+                "email": thForm['email'].value,
+                "phone": thForm['phone'].value,
+                "dept": thForm['dept'].value,
+                "batch": batch
+            }
+
+            console.log(JSONData)
+
+            const options = {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(JSONData)
+            };
+            fetch('https://member-details-api.herokuapp.com/', options);
+        }
+        else{
+            console.log("mail error");
+        }
     }
-
-    var JSONData = {
-        "memid": thForm['memid'].value,
-        "name": thForm['name'].value,
-        "email": thForm['email'].value,
-        "phone": thForm['phone'].value,
-        "dept": thForm['dept'].value,
-        "batch": batch
+    else{
+        console.log("ID error");
+        console.log(thForm['memid'].value.length);
     }
-
-    console.log(JSONData)
-
-    const options = {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(JSONData)
-    };
-    fetch('https://member-details-api.herokuapp.com/', options);
 }
 
