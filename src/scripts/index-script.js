e661261eee591364a018e70e7fb32f11e348c5aa
reenphygeorge <reenphygeorge@gmail.com>
2022-07-03 08:04:01 +0530

    fix: navbar && enhancment: file-structure

---
+++ src/scripts/index-script.js
@@ -0,0 +1,20 @@
+function display(option) {
+    if (option === 'yes') {
+        document.getElementById('main-page').classList.add('d-none')
+        document.getElementById('yes-page').classList.remove('d-none')
+    }
+    else if (option === 'no') {
+        document.getElementById('main-page').classList.add('d-none')
+        document.getElementById('no-page').classList.remove('d-none')
+    }
+    else if (option === 'confused') {
+        document.getElementById('main-page').classList.add('d-none')
+        document.getElementById('confused-page').classList.remove('d-none')
+    }
+    else if (option === 'back') {
+        document.getElementById('main-page').classList.remove('d-none')
+        document.getElementById('yes-page').classList.add('d-none')
+        document.getElementById('no-page').classList.add('d-none')
+        document.getElementById('confused-page').classList.add('d-none')
+    }
+}
