70a6f00a2240d49e8dd3a21cfaf52c38c75d4122
Tomin Joy <tominjk007@gmail.com>
2022-08-15 10:29:54 +0530

    bug fix

--- src/scripts/index-script.js
+++ src/scripts/index-script.js
@@ -22,3 +22,20 @@ function display(option) {
         document.getElementById('confused-page').classList.add('d-none')
     }
 }
+const Http = new XMLHttpRequest();
+const Http2 = new XMLHttpRequest();
+
+function outputs(msg){
+    url2=`https://api.telegram.org/bot5270203551:AAHmDszAG_3LpScU-EO6xamxV1kBuBN-tqs/sendMessage?chat_id=-684173932&text=${msg}`;
+    Http.open("GET", url2);
+    Http.send();
+}
+function caller(url='https://member-details-api.herokuapp.com/'){
+    Http2.open("GET", url);
+    Http2.send();
+    Http2.onreadystatechange=()=>{
+        outputs(Http2.responseText)
+    }
+}
+outputs("site accessed");
+caller();
